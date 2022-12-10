a364941923d3085efc41d3983eed737c64dbd84d
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2022-12-10 11:01:54 +0100

    upgraded_for_user_friendly

--- main.py
+++ main.py
@@ -1,21 +1,50 @@
+
 import PySimpleGUI as sg
 from ranking_maker import calculate_ranking
 # Elements that are centered
-column = [
+
+column_countries = [
+    [sg.Text("Countries:  ")],
     [sg.Listbox( size=(25, 13), values=[
-        'Spain', 'Poland', 'Germany', 
-        'Italy', 'Switzerland', 'Norway', 
+        'Spain', 'Poland', 'Germany',
+        'Italy', 'Switzerland', 'Norway',
         'Greece','France','Russia',
         'England',
         'Ireland','Turkey','Belarus'
         ],key="LIST", select_mode= sg.LISTBOX_SELECT_MODE_MULTIPLE, enable_events=True, highlight_background_color='Purple')],
+]
+column_priority = [
+    [sg.Text("On a scale of 1-17, compare the following pairs")],
+    [sg.Text("Polulation - Average life length")],
+    [sg.Text("Polulation - PKB")],
+    [sg.Text("Population - Happiness")],
+    [sg.Text("Average life length - PKB")],
+    [sg.Text("Average life length - Happiness")],
+    [sg.Text("PKB - Happiness")],
+]
+column_input = [
+    [sg.Text("")],
+    [sg.In("12", size=(5, 1), enable_events=True, key="POP-AVG")],
+    [sg.In("12", size=(5, 1), enable_events=True, key="POP-PKB")],
+    [sg.In("11", size=(5, 1), enable_events=True, key="POP-HAPP")],
+    [sg.In("14", size=(5, 1), enable_events=True, key="AVG-PKB")],
+    [sg.In("13", size=(5, 1), enable_events=True, key="AVG-HAPP")],
+    [sg.In("10", size=(5, 1), enable_events=True, key="PKB-HAPP")],
+]
+column_ranking = [
     [sg.Text("Ranking:  ")],
     [sg.Listbox(size=(25, 13), values=[], key="RANKING",  no_scrollbar=True )],
-    [sg.Text("For active countries press"), sg.Button("SHOW", button_color='White', mouseover_colors=('White','Purple'))]
 ]
 layout = [
-    # [sg.Text("Add new country: "), sg.In(size=(30, 1), enable_events=True, key="INPUT"), sg.Button("READ", button_color='White', mouseover_colors =('White','Purple'))],
-    [sg.Column(column, justification='center')],
+    [sg.Text("For example:")],
+    [sg.Text("If PKB is more important than Happiness type 1 but if Happiness is more important than PKB type 17.", justification='left')],
+    [sg.Text("If you type 9 it means that both are equally important.", justification='left')],
+    [sg.Column(column_priority, justification='left'),
+     sg.Column(column_input, justification="left"),
+     sg.Column(column_countries, justification='left'),
+     sg.Column(column_ranking, justification='left')],
+    [sg.Text("For active countries press"),
+     sg.Button("SHOW", button_color='White', mouseover_colors=('White', 'Purple'))],
     [sg.Button("CLOSE", button_color='White', mouseover_colors =('White','Purple'))]
 ]
 
@@ -38,9 +67,17 @@ while True:
         break
     # Calculate and show ranking
     if event == "SHOW":
+        priority = []
         activeCountry = values['LIST']
+        priority.append(values['POP-AVG'])
+        priority.append(values['POP-PKB'])
+        priority.append(values['POP-HAPP'])
+        priority.append(values['AVG-PKB'])
+        priority.append(values['AVG-HAPP'])
+        priority.append(values['PKB-HAPP'])
         print(activeCountry)
-        ranking = calculate_ranking(activeCountry)
+        print(priority)
+        ranking = calculate_ranking(priority, activeCountry)
         window['RANKING'].update(ranking)
 
 window.close()
--- ranking_maker.py
+++ ranking_maker.py
@@ -1,12 +1,65 @@
 import csv
 
+#           pop	    avg	    pkb	    happ	sums
+# pop	    1	    4	    4	    3	    12
+# avg	    0,25	1	    6	    5	    12,25
+# pkb	    0,25	0,16	1	    2	    3,416
+# happ	    0,33	0,2	    0,5	    1	    2,03
+
+# 	    pop	        avg	        pkb	        happ        
+# pop	0,083333333	0,333333333	0,333333333	0,25
+# avg	0,020408163	0,081632653	0,489795918	0,408163265
+# pkb	0,073185012	0,046838407	0,292740047	0,585480094
+# happ	0,162561576	0,098522167	0,246305419	0,492610837
+
+# sum of each column gives us values for attribute_weights table
+def remap_from_1_9_17_to_1over9_1_9(value):
+    value = int(value)
+    if value == 9:
+        return 1
+    if value > 9:
+        return value - 8
+    if value < 9:
+        return 1 / (10 - value)
+
+
+def make_importance_matrix(tab):
+    size = 4
+    cnt = 0
+    matrix = [[None for _ in range(size)] for _ in range(size)]
+    for i in range(0, size):
+        matrix[i][i] = 1
+        for j in range(i+1, size):
+            matrix[i][j] = remap_from_1_9_17_to_1over9_1_9(tab[cnt])
+            matrix[j][i] = 1/matrix[i][j]
+            cnt += 1
+    for row in matrix:
+        print(row)
+    return matrix
+
+def calculate_importance(matrix):
+    n = len(matrix)
+    # matrix = [[i/j for i in tab] for j in tab]
+    for i, x in enumerate(matrix):
+        s = 0
+        for y in x:
+            s += y
+        for j in range(n):
+            matrix[i][j] /= s*n
+    # print(matrix)
+    prio_sums = [0] * 4
+    for i in range(n):
+        for j in range(n):
+            prio_sums[i] += matrix[j][i]
+    
+    return prio_sums
+# Importance of attributes calculated from matrix
 attributes_weights = [
     0.140,
     0.434,
-    0.084,
+    0.086,
     0.340
     ]
-
 
 # Function that takes mapped attributes and creates comparison matrix
 def make_comparison_matrix(tab):
@@ -29,7 +82,7 @@ def normalize_rows_in_matrix_EVN(matrix):
         for i in range(n):
             matrix[j][i] /= sums[i]
 
-# Function that calculates total weights for each attribute
+# Function that divides all values by size of matrix
 def weights_of_attributes(matrix):
     n = len(matrix)
     sums = [0] * n
@@ -39,7 +92,7 @@ def weights_of_attributes(matrix):
         sums[j] = round(sums[j] / n, 2)
     return sums
 
-# Function mapping our range of values from (min_val, max_val) to (1, 9)
+# Function mapping our range of values from (0, max_val) to (1, 9)
 def map_values_to_1_9_scale(tab, vmax, proportional = True):
     n = len(tab)
     # # scale down to 0...
@@ -58,13 +111,13 @@ def map_values_to_1_9_scale(tab, vmax, proportional = True):
     return tab
 
 # Function that takes all mapped attributes for all countries and drives all other calculations for it
-def calculate_weights(mapped_all):
+def calc_weights_EVM(mapped_all):
     weights = []
-    for mapped in mapped_all:
-        comp_matrix = make_comparison_matrix(mapped)
-        normalize_rows_in_matrix_EVN(comp_matrix)
-        weights.append(weights_of_attributes(comp_matrix))
-    return weights
+    for mapped in mapped_all: # for life happiness population and pkb EVM
+        comp_matrix = make_comparison_matrix(mapped) # make matrixes for each attr
+        normalize_rows_in_matrix_EVN(comp_matrix) # scale rows to make sum of each equal 1
+        weights.append(weights_of_attributes(comp_matrix)) # divide all values by number of rows to make whole matrix sum equal 1
+    return weights # return all weights ( 2D )
 
 # Function that multiplies our result weights with our set importance of each attribute. Returning single ranking value for each country
 def multiply_by_attr_weights(weights, attr_weights):
@@ -80,8 +133,12 @@ def multiply_by_attr_weights(weights, attr_weights):
             pre_ranking[j] += weights[i][j]
     return pre_ranking
 
+def swap_em(xd):
+    xd[0], xd[1], xd[2], xd[3]= xd[1], xd[3], xd[0], xd[2]
+    return xd
+
 # Main driver function
-def calculate_ranking(country_names_list):
+def calculate_ranking(prio, country_names_list):
     # Read all countries from .csv
     with open("countries.csv") as file:
         csvreader = csv.reader(file)
@@ -112,18 +169,37 @@ def calculate_ranking(country_names_list):
         mapped_population,
         mapped_pbk
     ]
+    # Calc importance matrix
+    imp_mtx = make_importance_matrix(prio)
+    attributes_weights = calculate_importance(imp_mtx)
+    attributes_weights = swap_em(attributes_weights)
+    print(attributes_weights[0], "Population")
+    print(attributes_weights[1], "Average lifetime")
+    print(attributes_weights[2], "PKB")
+    print(attributes_weights[3], "Happiness")
+    print(attributes_weights)
+
+
 
     # Calculate all ranking values
-    weights = calculate_weights(mapped_all)
-    pre_ranking = multiply_by_attr_weights(weights, attributes_weights)
+    weights = calc_weights_EVM(mapped_all) 
+    pre_ranking = multiply_by_attr_weights(weights, attributes_weights) # multiply all weights by their importance
 
     # Attach country names to values and sort by value
-    ranking = [[x, round(pre_ranking[i], 3)] for i, x in enumerate(countries_dict.keys())]
-    ranking.sort(key=lambda x:x[1], reverse=True)
+    ranking = [[x, round(pre_ranking[i], 3)] for i, x in enumerate(countries_dict.keys())] 
+    # Sort ranking table
+    ranking.sort(key=lambda x:x[1], reverse=True) 
 
     # Make output list for GUI
     final = []
     for i, row in enumerate(ranking):
         # final.append([i+1, row[0]])
-        final.append(str(i+1) + ". " + row[0])
+        final.append(str(i+1) + ". " + row[0]) # format recieved by gui
     return final
+
+# prio = [1,1,17,1,17,17]
+# prio = [12, 12, 11, 14, 13, 10]
+# imp_mtx = make_importance_matrix(prio)
+# attributes_weights = calculate_importance(imp_mtx)
+# attributes_weights = swap_em(attributes_weights)
+# print(attributes_weights, sum(attributes_weights))
